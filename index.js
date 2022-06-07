e62b8cf507294848ccb8658a9f6209316a238df1
larrylizhao <larrylizhao@gmail.com>
2022-06-07 22:37:28 +0800

    Update logic for checking

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const Koa = require('koa');
+const Router = require('@koa/router');
 const app = new Koa();
+const router = Router();
+const alto = require('./alto');
 
-app.use(async (ctx, next) => {
-    await next();
-    ctx.response.type = 'text/html';
-    ctx.response.body = '<h1>Hello, koa2!</h1>';
-})
+// add url-route:
+router.get('/check/:date', async (ctx, next) => {
+    var date = ctx.params.date;
+    ctx.response.body = alto.getSlots(date);
+});
+
+router.get('/', async (ctx, next) => {
+    ctx.response.body = '<h1>Index</h1>';
+});
+
+// add router middleware:
+app.use(router.routes())
+    .use(router.allowedMethods());
 
 // no need for `app.listen()` on Deta, they run the app automatically.
 module.exports = app; // make sure to export `app` instance.
